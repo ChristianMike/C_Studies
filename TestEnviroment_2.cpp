9b440b2ae31a756e1dd49c5b42ad75dc5a9d68cd
ChristianMike <eriksson.mikekristian@gmail.com>
2022-02-15 06:09:55 +0000

    Create TestEnviroment_2.cpp
    Just testing more

---
+++ TestEnviroment_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#include <cmath>;
+
+int main() {
+	int sum1 = 100 + 50;
+	int sum2 = 200 + 60;
+	int sum3 = 300 + 70;
+	int sum4 = 400 + 80;
+
+	int sumFor = sum1 + sum2;
+	int sumForAnother = sum3 + sum4;
+
+	int sumForsum = sumFor + sumForAnother;
+
+	cout << sum1 << "\n";
+	cout << sum2 << "\n";
+	cout << sum3 << "\n";
+	cout << sum4 << "\n";
+
+	cout << sumFor << "\n";
+	cout << sumForAnother << "\n";
+	cout << sumForsum << "\n";
+
+	return 0;
+
+}
